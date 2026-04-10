08b9db78d4a90ff9b1c3a147c9fba89937ca85e6
UJALA-PATEL <Patelujala448@gmail.com>
2026-04-10 22:54:56 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API =
   process.env.NODE_ENV === "production"
-    ? "https://your-backend.onrender.com"
+    ? "https://task-manager-backend-i6qm.onrender.com"
     : "http://localhost:5000";
 
 export default API;
