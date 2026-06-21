8845810a4482d9553d1f86338689fa30e12f62fb
UJALA-PATEL <patelujala448@gmail.com>
2026-06-21 12:05:31 +0530

    chnages done

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://smart-task-management-application-1.onrender.com"
 });
 
 // token attach automatically
